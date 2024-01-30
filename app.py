459ef6e382e5ed6dc0b8066b1ca6af2827b55af7
Umidjon12346 <umidshari456@gmail.com>
2024-01-30 16:08:49 +0500

    done

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask,request
 import telegram
 
+Token = "6745554947:AAH0Xt-pNF7dasRCeI2BjgMuXN-1o-DzFbk"
+bot = telegram.Bot(Token)
+chat_id = "1806482236"
 app = Flask(__name__)
 
 @app.route("/")
 def user():
+    bot.send_message(chat_id,"hello world")
     return "HEllo"
 
 if __name__=="__main__":
---
+++ bot.py
@@ -0,0 +1,6 @@
+import telegram
+
+Token = "6745554947:AAH0Xt-pNF7dasRCeI2BjgMuXN-1o-DzFbk"
+bot = telegram.Bot(Token)
+Url = "https://umidjon12346.pythonanywhere.com/"
+bot.setWebhook(Url)
